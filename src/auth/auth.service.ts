1e3509f9796001ebebcfce65cea9d93efdbbdb87
teodorat63 <teodoratomic300@gmail.com>
2025-03-20 15:47:22 +0000

    Worked on signing in and signing up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import * as argon from 'argon2';
 @Injectable({})
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
-  signup() {
-    return { msg: 'I have signed up' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    //instalirati argon
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
-  signin() {
-    return { msg: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    //find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //if user doesnt exist throw exceptions
+    if (!user) throw new ForbiddenException('User doesnt exist');
+    //compare passwords
+    const pwMatches = await argon.verify(user.password, dto.password);
+    //if passport incorect tthrow exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Wrong credentials');
+    }
+    //send back user
+    //delete user.password;
+    return user;
   }
 }
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
